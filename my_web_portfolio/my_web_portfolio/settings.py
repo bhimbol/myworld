8a1248838720e6a554b0f75387832a43667e80dc
bhimbol <buggycode25@gmail.com>
2023-11-09 15:15:18 +0000

    commenting 123

--- my_web_portfolio/my_web_portfolio/settings.py
+++ my_web_portfolio/my_web_portfolio/settings.py
@@ -120,14 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-PYTHONANYWHERE_STATIC_DIR = '/home/airbytes/myworld/my_web_portfolio/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    PYTHONANYWHERE_STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
