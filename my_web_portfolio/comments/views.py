8ca3fd2d7e3d94be0619dc6bcbb34c8dada4fce0
bhimbol <buggycode25@gmail.com>
2023-11-10 10:03:30 +0000

    comment

--- my_web_portfolio/comments/views.py
+++ my_web_portfolio/comments/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, redirect
+from .models import Comment
+from .forms import CommentForm
 
 def comments(request):
-	template = loader.get_template('comments.html')
-	return HttpResponse(template.render())
+    comments_list = Comment.objects.all()
+    form = CommentForm()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('comments')
+    return render(request, 'comment.html', {'comments_list': comments_list, 'form': form})
